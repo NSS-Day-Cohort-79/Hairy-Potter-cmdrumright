ac137b59c97d7824db7f392838807f7eb77f0fca
cmdrumright <cdrumright@protonmail.com>
2025-11-17 13:27:23 -0600

    added PotteryList.js which exports potteryList()

---
+++ src/scripts/PotteryList.js
@@ -0,0 +1,17 @@
+import { usePottery } from "./PotteryCatalog.js"
+
+export const potteryList = () => {
+    potteryHTML = ""
+    for (const sellItem of usePottery()) {
+        potteryHTML += `
+            <section class="pottery" id="pottery--${sellItem.id}">
+              <h2 class="pottery__shape">${sellItem.shape}</h2>
+              <div class="pottery__properties">
+                Item weighs ${sellItem.weight} and is ${sellItem.height} in height
+              </div>
+              <div class="pottery__price">Price is ${sellItem.price}</div>
+            </section>
+        `
+    }
+    return potteryHTML;
+}
